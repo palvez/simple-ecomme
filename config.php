2eb81520cfc4b1416235b6146b882fcbff48e07d
palvez <69176838+palvez@users.noreply.github.com>
2021-05-17 10:31:25 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', 'db-relacional.red-interna-yp.internal');
+	define('DB_HOST', 'hostdb');
     define('DB_USER', 'user');
     define('DB_PASSWORD', 'password');
     define('DB_DATABASE', 'main');
