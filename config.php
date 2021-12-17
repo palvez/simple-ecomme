5eeef1c28f091c5ac174bf0d664f12ab1b072184
palvez <69176838+palvez@users.noreply.github.com>
2021-12-17 14:10:02 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', '$DB_HOST');
-    define('DB_USER', '$DB_USER');
-    define('DB_PASSWORD', '$DB_PASSWORD');
-    define('DB_DATABASE', '$DB_DATABASE');
+	define('DB_HOST', '"$DB_HOST"');
+    define('DB_USER', '"$DB_USER"');
+    define('DB_PASSWORD', '"$DB_PASSWORD"');
+    define('DB_DATABASE', '"$DB_DATABASE"');
 ?>
