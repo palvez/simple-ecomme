6eea64b0e57ec76d3947d19fdb308c23483e38c4
palvez <69176838+palvez@users.noreply.github.com>
2021-12-17 19:40:53 -0300

    Update config.php

--- config.php
+++ config.php
@@ -7,6 +7,6 @@
 	error_reporting(-1);
     define('DB_HOST', $dbhost );
     define('DB_USER', $user );
-    define('DB_PASSWORD', $user );
+    define('DB_PASSWORD', $password );
     define('DB_DATABASE', $dbdatabase );
 ?>
