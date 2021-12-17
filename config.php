d861f226a408e9c0330e0245ba84a532839d96fe
palvez <69176838+palvez@users.noreply.github.com>
2021-12-17 09:02:00 -0300

    Cambio de variables de config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', 'hostdb');
-    define('DB_USER', 'user');
-    define('DB_PASSWORD', 'password');
-    define('DB_DATABASE', 'main');
+	define('DB_HOST', '$DB_HOST');
+    define('DB_USER', '$DB_USER');
+    define('DB_PASSWORD', '$DB_PASSWORD');
+    define('DB_DATABASE', '$DB_DATABASE');
 ?>
